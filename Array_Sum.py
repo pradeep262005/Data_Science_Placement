184762b692028938e60598e6f45640e878cf46c9
pradeep262005 <jpradeepavt@gmail.com>
2025-04-21 20:05:53 +0530

    Changes made

--- Array_Sum.py
+++
@@ -1,7 +0,0 @@
-arr=[1,2,3,4]
-sum=0
-
-for i in range(0,len(arr)):
-    sum=sum+arr[i]
-
-print('sum',sum)
--- Count_Tuple.py
+++
@@ -1,8 +0,0 @@
-num = [10,20,30,135,76,(10,20),40]
-ctr = 0
-for n in num:
-    if isinstance(n, tuple):
-        break
-    ctr += 1
-
-print('Count of elements in a list until element is a tuple :',ctr)
